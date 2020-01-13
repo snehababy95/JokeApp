dd798213f40b6f2532cdd2a6fe7c86ead59920a6
Sneha <sneha.alex@attinadsoftware.com>
2020-01-13 07:10:19 +0530

    Updates

---
+++ precache-manifest.679e8b1892dfc06025c3000eb62f7f12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c59268512c3e0a68050b4c4554a9e2",
+    "url": "/JokeApp/index.html"
+  },
+  {
+    "revision": "18dfb5c09b4391fed261",
+    "url": "/JokeApp/static/css/main.5e5d0bb4.chunk.css"
+  },
+  {
+    "revision": "cfe2b5f36e852929cc52",
+    "url": "/JokeApp/static/js/2.db317e1d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/JokeApp/static/js/2.db317e1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18dfb5c09b4391fed261",
+    "url": "/JokeApp/static/js/main.ee6ffeb5.chunk.js"
+  },
+  {
+    "revision": "6ecf9011591e749ae24c",
+    "url": "/JokeApp/static/js/runtime-main.2be659a7.js"
+  },
+  {
+    "revision": "95baf3996494af9e4e11f46a84265e08",
+    "url": "/JokeApp/static/media/joke-theme.95baf399.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JokeApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d4f3e3e88b5e2c46181a5fd2314fabe6",
+    "url": "/JokeApp/static/media/theme.d4f3e3e8.jpg"
+  }
+]);
